def1b3cc301398875b43fc52e50ed936302f56c6
louis <louis.braun@gmx.de>
2014-11-29 18:16:34 +0100

    Version 0.0.8

--- skindesigner.c
+++ skindesigner.c
@@ -19,7 +19,7 @@
 #endif
 
 
-static const char *VERSION        = "0.0.7";
+static const char *VERSION        = "0.0.8";
 static const char *DESCRIPTION    = "SkinDesigner";
 static const char *MAINMENUENTRY  = "Skin Designer";
 
